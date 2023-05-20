4fcedbcb227eb8cdebccb0c3c03b0a04baccd9f1
ftovar8 <felix.godo@gmail.com>
2023-05-20 14:27:11 -0400

    update backend 5/20/2023

--- start.js
+++ start.js
@@ -6,19 +6,16 @@ Object.defineProperty(exports, "__esModule", {
 exports.__beyond_transversal = void 0;
 var dependency_0 = require("@beyond-js/kernel/bundle");
 var dependency_1 = require("@beyond-js/kernel/transversals");
-var dependency_2 = require("express");
-var dependency_3 = require("@aimpact/backend/routes");
-var dependency_4 = require("vhost");
-var dependency_5 = require("https");
-var dependency_6 = require("fs");
-var dependency_7 = require("dotenv");
-var dependency_8 = require("@beyond-js/backend/listen");
+var dependency_2 = require("@beyond-js/backend/listen");
+var dependency_3 = require("express");
+var dependency_4 = require("@aimpact/backend/routes");
+var dependency_5 = require("dotenv");
 const {
   Transversal
 } = brequire('@beyond-js/kernel/transversals');
 const __beyond_transversal = new Transversal('start', '');
 exports.__beyond_transversal = __beyond_transversal;
-__beyond_transversal.dependencies.update([['@beyond-js/kernel/transversals', dependency_1], ['express', dependency_2], ['@aimpact/backend/routes', dependency_3], ['vhost', dependency_4], ['https', dependency_5], ['fs', dependency_6], ['dotenv', dependency_7], ['@beyond-js/backend/listen', dependency_8]]);
+__beyond_transversal.dependencies.update([['@beyond-js/kernel/transversals', dependency_1], ['@beyond-js/backend/listen', dependency_2], ['express', dependency_3], ['@aimpact/backend/routes', dependency_4], ['dotenv', dependency_5]]);
 const bundles = [];
 /******************************
 MODULE: @aimpact/backend/server
@@ -68,7 +65,7 @@ bundles.push([{
   ************************/
 
   ims.set('./server', {
-    hash: 282955686,
+    hash: 3947523807,
     creator: function (require, exports) {
       "use strict";
 
@@ -76,55 +73,39 @@ bundles.push([{
         value: true
       });
       exports.Server = void 0;
+      var _listen = require("@beyond-js/backend/listen");
       var express = require("express");
       var _connections = require("./connections");
       var _routes = require("@aimpact/backend/routes");
-      var vhost = require("vhost");
-      var https = require("https");
-      var _fs = require("fs");
       var dotenv = require("dotenv");
       dotenv.config();
       class Server {
         #instance;
         #connections;
         #app;
-        #port = process.env.EXPRESS_PORT;
+        #port = process.env.PORT || 5000;
         #router;
         constructor() {
           this.#start();
         }
         #start() {
           try {
-            const app = express();
-            this.#setHeader(app);
+            this.#app = express();
+            this.#setHeader();
             this.#router = express.Router();
-            (0, _routes.routes)(app);
-            //-------------------
-            const prod = express();
-            let server;
-            console.log(1, process.env.NODE_ENV);
-            if (process.env.NODE_ENV === "production") {
-              // In production, use the process.env.SUBDOMAIN_URL subdomain
-              prod.use(vhost(process.env.SUBDOMAIN_URL, app));
-              const options = {
-                key: (0, _fs.readFileSync)("/etc/letsencrypt/live/dev.backend.aimpact.partners/fullchain.pem"),
-                cert: (0, _fs.readFileSync)("/etc/letsencrypt/live/dev.backend.aimpact.partners/privkey.pem")
-              };
-              server = https.createServer(options, prod).listen(this.#port);
-            }
-            this.#app = process.env.NODE_ENV === "production" ? prod : app;
+            (0, _routes.routes)(this.#app);
             this.#app.use(express.json());
-            //-------------------
-            this.#instance = process.env.NODE_ENV === "production" ? server : this.#app.listen(this.#port);
+            this.#instance = this.#app.listen(this.#port);
             this.#connections = new _connections.Connections(this.#instance);
+            (0, _listen.listen)(this.#instance);
             //subscription to listen routes module changes.
             _routes.hmr.on("change", this.onChange);
           } catch (exc) {
             console.error("Error", exc);
           }
         }
-        #setHeader(app) {
-          app.use((req, res, next) => {
+        #setHeader() {
+          this.#app.use((req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method");
             res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
@@ -158,42 +139,7 @@ bundles.push([{
     }
   });
   return {
-    dependencies: ['express', '@aimpact/backend/routes', 'vhost', 'https', 'fs', 'dotenv']
-  };
-}]);
-
-/*************************************
-MODULE: @aimpact/backend/start-backend
-*************************************/
-
-bundles.push([{
-  "module": {
-    "vspecifier": "@aimpact/backend@1.0.0/start-backend"
-  },
-  "type": "start"
-}, function (ims, exports) {
-  /***********************
-  INTERNAL MODULE: ./start
-  ***********************/
-
-  ims.set('./start', {
-    hash: 3995412763,
-    creator: function (require, exports) {
-      "use strict";
-
-      var _listen = require("@beyond-js/backend/listen");
-      var dotenv = require("dotenv");
-      /*
-       * Initialize library beyondJS backend server
-       */
-
-      dotenv.config();
-      (0, _listen.listen)(6530);
-      console.log(process.env.OPEN_AI_KEY);
-    }
-  });
-  return {
-    dependencies: ['@beyond-js/backend/listen', 'dotenv']
+    dependencies: ['@beyond-js/backend/listen', 'express', '@aimpact/backend/routes', 'dotenv']
   };
 }]);
 __beyond_transversal.initialise(bundles);
