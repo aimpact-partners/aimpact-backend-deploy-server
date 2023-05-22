c73b662acd25eabc2cab55fe0382da4b29a6df07
ftovar8 <felix.godo@gmail.com>
2023-05-22 09:57:20 -0400

    clean code v1-5/22/2023

--- index.js
+++
@@ -1,5 +0,0 @@
-globalThis.__app_package = {specifier: '@aimpact/backend'};
-Object.defineProperty(globalThis, 'brequire', {get: () => require});
-Object.defineProperty(globalThis, 'bimport', {get: () => async specifier => require(specifier)});
-
-require('./start.js');
